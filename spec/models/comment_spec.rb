0bb293b56a3d7c8874a04680b9f58c7077a00063
Shingo Kobayashi <sngk0325@gmail.com>
2018-10-30 02:22:48 +0000

    made some tests for model of comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include LoginSupport
+  let(:user) { factoryBot.create(:user) }
+  
+  describe "create a comment" do
+    context "when a comment is invalid" do
+      it "because of no any information" do
+        expect(Comment.create()).to be_invalid
+      end
+      
+      it "because of lack of comment" do
+        expect(Comment.create(
+          user_id: "1",
+          post_id: "1",
+          )
+        ).to be_invalid
+      end
+      
+      it "because of over limit of characters limitation" do
+        expect(Comment.create(
+          user_id: "1",
+          post_id: "1",
+          comment: "#{"a" * 141}")
+        ).to be_invalid
+      end
+      
+    end
+  end
+  
 end
