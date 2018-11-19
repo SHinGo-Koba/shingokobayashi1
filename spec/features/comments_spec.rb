bdcc24ee92881ce4c4785e3a5c948a77df97141c
Shingo Kobayashi <sngk0325@gmail.com>
2018-11-19 01:40:46 +0000

    added test codes

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -3,17 +3,23 @@ require 'rails_helper'
 RSpec.feature "Comments", type: :feature do
   include LoginSupport
   let!(:user1){ FactoryBot.create(:user) }
-  let(:post1){ FactoryBot.create(:post, user_id: user1.id) }
+  let!(:post1){ FactoryBot.create(:post, user_id: user1.id) }
   let!(:user2){ FactoryBot.create(:user) }
   
   scenario "user comments on a post by another user" do
     expect {
       login_as(user2, false)
       find("div.header-pc").find_link("NOW!").click
-      fill_in "comment on this post", with: "great!"
-      click.button "Comment!"
+      find("div.posts-each").find_link("test1").click
+      expect(page).to have_current_path post_path(1)
       
-      expect(page).to have_content "just commented"
+      fill_in "comment[comment]", with: "great!"
+      click_button "Comment!"
+      
+      expect(page).to have_content "Commented!"
+      
     }.to change(Comment, :count).by(1)
+    expect(page).to have_current_path post_path(1)
+    expect(page.body).to include("great!")
   end
 end
